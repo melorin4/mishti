a39c2785ce0f4090b1d74a7ace4bb91df806381b
selingl <selin.gilgil@gmail.com>
2023-05-06 19:02:46 +0300

    Interface added

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
 
-public class Player {
+public abstract class Player implements Moves {
     Game game = new Game();
     public ArrayList<Card> hand;
     public String name;
     public int points;
+
+
+
+    @Override
+    public void throwCard(int a){
+
+    }
 
 
 }
