7090cf2d93e70d88fb30c5a04c3f9ed0f5d7adcb
melorin <lorinerol257@gmail.com>
2023-05-08 19:10:02 +0300

    updated Card.java&Game.java
    added point reading

--- src/Card.java
+++ src/Card.java
@@ -1,13 +1,16 @@
+import java.util.List;
+
 public class Card {
     public String suit;
     public String value;
     public String cardName;
     public int cardPoint;
 
-    public Card(String suit, String value) {
+    public Card(String suit, String value, int cardPoint) {
         this.suit = suit;
         this.value = value;
         this.cardName = suit + " " + value;
+        this.cardPoint = cardPoint;
     }
 
     public String getSuit() {
@@ -21,6 +24,18 @@ public class Card {
     public String getValue() {
         return value;
     }
-}
 
-// file reading
+    public int getCardPoint() {
+        return cardPoint;
+    }
+
+    public int getPointValue(List<String> pointValues) {
+        for (String line : pointValues) {
+            String[] parts = line.split(" ");
+            if (parts[0].equals(suit) && (parts[1].equals(value) || parts[1].equals("*"))) {
+                return Integer.parseInt(parts[2]);
+            }
+        }
+        return 1; //default value for cards
+    }
+}
